231273d93356f3f7e465a9906f59899cdd97dd17
Pifou <48999464+Pif0u@users.noreply.github.com>
2020-09-22 11:59:54 +0200

    Exercice 4 : Tableau d'articles

---
+++ Excercice4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Excercice4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var article1 = new ArticleType("PS5", 499.99, 5, TypeArticleEnum.Loisir);
+            var article2 = new ArticleType("Jus d'" + "orange", 1.50, 25, TypeArticleEnum.Alimentaire);
+            var article3 = new ArticleType("Jeans", 15.0, 7, TypeArticleEnum.Habillement);
+
+            var tableauArticle = new ArticleType[] {article1, article2, article3};
+
+          
+            for(int i = 0; i < tableauArticle.Length; i++)
+            {
+                tableauArticle[i].Afficher();
+            }
+
+
+            Console.WriteLine("[Modification quanité Jeans]");
+            tableauArticle[2].Retirer();
+            tableauArticle[2].Afficher();
+
+
+
+
+
+        }
+    }
+}
