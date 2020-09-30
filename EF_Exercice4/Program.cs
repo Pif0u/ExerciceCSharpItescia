97400c324c2f4f849c20936c7fe0066b4c8777f1
Pifou <48999464+Pif0u@users.noreply.github.com>
2020-09-30 10:31:04 +0200

    Truc inutile

--- EF_Exercice4/Program.cs
+++ EF_Exercice4/Program.cs
@@ -8,9 +8,6 @@ namespace EF_Exercice4
         static void Main(string[] args)
         {
             Util.InitTable();
-            IArticleDao dao = new ArticleDao();
-            IEnumerable<Article> res = dao.GetArticleMinMax(11, 14);
-            Console.WriteLine(Environment.NewLine);
         }
     }
 }
